f7287dc6d1cf304cb32cb1139e0da4ed8c434004
John Paulo Guimalan <john.guimalan@RD-JGUIMALAN-MNL.local>
2025-11-11 22:16:30 +0800

    feat: Models for llm app has been registered on admin.py

--- llm/admin.py
+++ llm/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Agent, Role, Tool
 
-# Register your models here.
+admin.site.register(Agent)
+admin.site.register(Role)
+admin.site.register(Tool)
